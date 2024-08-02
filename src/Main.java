e1823f47952df2455cbd8a4a69ed266ff08e1709
D070508T <davidt070508@gmail.com>
2024-08-02 18:04:25 -0400

    First Version

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
+        PiggyBank bank = new PiggyBank();
 
+        bank.setName("bank1");
+
+        System.out.println(bank.getName());
     }
 }
--- src/PiggyBank.java
+++ src/PiggyBank.java
@@ -1,2 +1,15 @@
 public class PiggyBank {
+    private String name;
+
+    public PiggyBank() {
+        name = "";
+    }
+
+    public void setName(String n) {
+        name = n;
+    }
+
+    public String getName() {
+        return name;
+    }
 }
