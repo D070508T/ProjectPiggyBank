db694916d0f6b8a01ea91b4f4f16c832ce8d774f
D070508T <davidt070508@gmail.com>
2024-08-03 17:42:06 -0400

    bug

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,16 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         Spending bank = new Spending();
+        Scanner scanner = new Scanner(System.in);
 
         bank.setCoins(new int[]{5, 5, 5, 5, 5});
 
-        for (int i : bank.withdrawCoins(0.81, new int[]{5, 5, 5, 5, 5})) {
-            System.out.println(i);
+        while (true) {
+            for (int i : bank.withdrawCoins(scanner.nextDouble(), new int[]{5, 5, 5, 5, 5})) {
+                System.out.println(i);
+            }
         }
     }
 }
