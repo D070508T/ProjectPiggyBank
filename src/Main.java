a577578b3ffc86a6bde4ec081ad252dc134e906a
D070508T <davidt070508@gmail.com>
2024-08-03 23:01:54 -0400

    bug

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,50 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Spending bank = new Spending();
-        Scanner scanner = new Scanner(System.in);
+        double cash = 0;
+        ArrayList<PiggyBank> userBanks = new ArrayList<>();
+        main_menu(userBanks, cash, new Scanner(System.in));
+    }
 
-        bank.setCoins(new int[]{5, 5, 5, 5, 5});
+    public static void main_menu(ArrayList<PiggyBank> userBanks, double cash, Scanner scanner) {
+        System.out.println(
+                "     -----<<< MAIN MENU >>>-----\n\n" +
+                        "Cash: $" + cash + "\n"
+        );
 
-        while (true) {
-            for (int i : bank.withdrawCoins(scanner.nextDouble(), new int[]{5, 5, 5, 5, 5})) {
-                System.out.println(i);
+        int amount = userBanks.size();
+
+        if (amount == 0) {
+            System.out.print("You do not own a Piggy Bank. Here is the catalogue of Piggy Banks");
+            catalogue(cash, scanner);
+        } else {
+            System.out.println("""
+                    <1>          Access Piggy Banks
+                    <2>              Open Catalogue
+                    <3>                        Quit
+                    """);
+
+            switch (scanner.nextLine()) {
+                case "1":
+                    displayBanks();
+                    break;
+                case "2":
+                    catalogue(cash, scanner);
+                    break;
+                case "3":
+                    System.out.println("Goodbye");
+                    System.exit(0);
             }
         }
+    }
+
+    public static void displayBanks() {
+
+    }
+
+    public static void catalogue(double cash, Scanner scanner) {
+
     }
 }
